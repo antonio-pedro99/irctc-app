2f509d770392049f510da275645e255ef2adaf9e
Antonio Pedro <tonio.pedro99@gmail.com>
2023-03-16 12:39:31 +0530

    improve endpoints

--- lib/app/services/endpoints.dart
+++ lib/app/services/endpoints.dart
@@ -1,4 +1,4 @@
-//ipconfig 192.168.51.161
+//change the domain to your machine's ip address or to 10.0.2.2 if you are running the app  on an emulator
 const String domain = "http://192.168.40.15:8000";
 const String authLogin = "$domain/auth/login/";
 const String authRegister = "$domain/auth/register";
